1c543192ac1d1493f03271d4845852114a7b973f
cphsiung <chunpeihs@gmail.com>
2020-06-22 23:16:14 -0400

    Add styles

--- assets/js/todos.js
+++ assets/js/todos.js
@@ -22,6 +22,10 @@ $("input[type='text']").keypress(function(event){
     //set text input back to empty
     $(this).val("");
     //create a new li and add to ul
-    $("ul").append("<li><span>X</span> " + todoText + "</li>");
+    $("ul").append("<li><span><i class='fa fa-trash'></i></span> " + todoText + "</li>");
   }
 });
+
+$(".fa-plus").click(function(){
+  $("input[type='text']").fadeToggle();
+})
